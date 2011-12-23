c32478fd242d5cf639e3223c9bea1946f4119114
Zydox <Zydox79@gmail.com>
2011-12-23 13:02:18 +0100

    Modified the debug function so it doesn't write anything to console until it's said to do so

--- debug.py
+++ debug.py
@@ -7,6 +7,8 @@ class Debug:
 	def __init__ (self):
 		i = 1
 		self.LogFile = None
+		self.LogStore = 1
+		self.LogHistory = []
 	
 	
 	def Debug (self, Info = ''):
@@ -14,12 +16,19 @@ class Debug:
 		filename = os.path.basename (frame.f_back.f_code.co_filename)
 		fileline = frame.f_back.f_lineno
 		function = frame.f_back.f_code.co_name
-		if self.LogFile:
-			file = open (self.LogFile, 'a')
-			file.write (time.strftime ('%Y%m%d %H:%M:%S') + '\t' + str (time.clock ()) + '\t' + filename + '\t' + function + '\t' + str (Info) + '\n')
-			file.close ()
+		LogLine = time.strftime ('%Y%m%d %H:%M:%S') + '\t' + str (time.clock ()) + '\t' + filename + '\t' + function + '\t' + str (Info) 
+		if self.LogStore:
+			self.LogHistory.append (LogLine)
+		elif self.LogFile:
+			self.WriteLogToFile (LogLine)
 		else:
-			print (time.strftime ('%Y%m%d %H:%M:%S') + '\t' + filename + '\t' + function + '\t' + str (Info))
+			print (LogLine)
+	
+	
+	def WriteLogToFile (self, Line):
+		file = open (self.LogFile, 'a')
+		file.write (Line + '\n')
+		file.close ()
 	
 	
 	def SetFile (self, LogFile):
@@ -27,3 +36,7 @@ class Debug:
 		file = open (self.LogFile, 'w')
 		file.close ()
 		self.Debug ('Log file set')
+		self.LogStore = 0
+		for Line in self.LogHistory:
+			self.WriteLogToFile (Line)
+		self.LogHistory = []

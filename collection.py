0e9d1b9d4e2663a348e9bebdf738b8db3ae3e042
Zydox <Zydox79@gmail.com>
2011-12-28 03:09:02 +0100

    Added a !terminate command and the bot keeps track if the data comes from the initial login burst or not

--- collection.py
+++ collection.py
@@ -18,13 +18,25 @@ class Collection:
 	
 	def Start (self):
 		self.Debug ()
-		self.Lobby = lobby.Lobby (self.Debug, self.Debug, self.Event, self.Config['General'])
+		self.Lobby = lobby.Lobby (self.Debug, self.Chat, self.Event, self.Config['General'])
 		self.Lobby.start ()
 	
 	
+	def Chat (self, Source, Data):
+		if Source == 'SAIDPRIVATE':
+			if Data[1] == '!terminate':
+				print 'Terminated by ' + str (Data[0])
+				self.Lobby.Terminate ()
+	
+	
 	def Event (self, Event, Data):
-		if Event == 'CLIENTSTATUS' and self.Lobby.Users[Data[0]]['InBattle']:
+		if Event == 'ACCEPTED':
+			self.InitLoad = 1
+		elif Event == 'LOGININFOEND':
+			self.InitLoad = 0
+		elif Event == 'CLIENTSTATUS' and self.Lobby.Users[Data[0]]['InBattle']:
 			print ""
+			print self.InitLoad
 			print Event
 			print Data
 			print self.Lobby.Users[Data[0]]

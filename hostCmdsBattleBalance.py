84795534c89b34e2e755b3a77b26ce52807883fb
Zydox <Zydox79@gmail.com>
2012-01-11 18:09:57 +0100

    New balance based on random sorting and Abma suggestion

--- hostCmdsBattleBalance.py
+++ hostCmdsBattleBalance.py
@@ -3,6 +3,7 @@ import time
 import math
 from operator import itemgetter
 import operator
+import random
 
 
 class HostCmdsBattleBalance:
@@ -56,6 +57,7 @@ class HostCmdsBattleBalance:
 		self.Data['OptimalTeamPlayers'] = int (math.floor (self.Players / float (self.Teams)))
 		
 		self.BalanceClans ()
+		self.BalanceTeams ()
 		self.BalancePlayers ()
 		
 		if self.DebugBalanceFunction:
@@ -98,36 +100,67 @@ class HostCmdsBattleBalance:
 					self.Balance.append ([User, Team + 1])
 	
 	
+	def BalanceTeams (self, IncludeEmptyTeams = 0):
+		for Team in self.TeamPlayers.keys ():
+			if (self.TeamPlayers[Team] or IncludeEmptyTeams) and self.TeamPlayers[Team] < self.Data['OptimalTeamPlayers']:
+				NeededPlayers = self.Data['OptimalTeamPlayers'] - self.TeamPlayers[Team]
+				NeededRank = self.Data['OptimalTeamRank'] - self.TeamRank[Team]
+				Players = self.BalanceGetPlayers (NeededPlayers, NeededRank)
+				for User in Players:
+					self.TeamPlayers[Team] += 1
+					self.TeamRank[Team] += self.PlayerList[User]
+					self.Balance.append ([User, Team + 1])
+					del (self.PlayerList[User])
+	
+	
+	def BalanceGetPlayers (self, GetPlayers, GetRank):
+		Best = GetRank
+		for iRand in range (0, 100):
+			random.seed()
+			Players = self.PlayerList.keys ()
+			iPlayers = GetPlayers
+			Users = []
+			Rank = 0
+			while iPlayers > 0:
+				User = Players.pop (random.randint( 0,len (Players) - 1))
+				iPlayers -= 1
+				Users.append (User)
+				Rank += self.PlayerList[User]
+			if Rank == GetRank:
+				Best = 0
+				BestUsers = Users[:]
+				break
+			elif abs (GetRank - Rank) < Best:
+				Best = abs (GetRank - Rank)
+				BestUsers = Users[:]		
+		return (BestUsers)
+	
+	
 	def BalancePlayers (self):
 		if len (self.PlayerList) == 0:
 			return (True)
-		
-		SortedUsers = {}
-		for User in self.PlayerList.keys ():
-			SortedUsers[User] = self.PlayerList[User]
-		SortedUsers = sorted(SortedUsers.items(), key=itemgetter(1), reverse=True)
-		
-		for User, Rank in SortedUsers:
-			Team = self.GetBestTeam (1, self.PlayerList[User])
-			self.TeamPlayers[Team] += 1
-			self.TeamRank[Team] += self.PlayerList[User]
-			del (self.PlayerList[User])
-			self.Balance.append ([User, Team + 1])
+		self.BalanceTeams (1)
 	
 	
 	def GetBestTeam (self, Players, Rank):
 		Teams = {}
 		for Team in self.TeamPlayers:
 			Teams[Team] = 0
+			"""	If a bunch of players are beeing placed and the searched team has no players,
+					it gets a bonus value of 100k...
+			"""
+			if not self.TeamPlayers[Team] and Players > 1:
+				Teams[Team] += 100000
+			
 			if self.TeamPlayers[Team] + Players == self.Data['OptimalTeamPlayers']:
 				Teams[Team] += 10000
 			elif self.TeamPlayers[Team] + Players < self.Data['OptimalTeamPlayers']:
 				Teams[Team] += 1000
 			else:
 				Teams[Team] += 100
-		
+		 
 		for Team in self.TeamRank:
-			if self.TeamRank[Team] + Rank == self.Data['OptimalTeamRank']:
+			if self.TeamRank[Team] + Rank == self.Data['OptimalTeamRank'] and self.TeamPlayers[Team] + Players == self.Data['OptimalTeamPlayers']:
 				Teams[Team] += 1000
 			elif self.TeamRank[Team] + Players < self.Data['OptimalTeamRank']:
 				Teams[Team] += 100
